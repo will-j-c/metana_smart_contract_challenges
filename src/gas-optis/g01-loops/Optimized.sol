4edc5cba6a1f5d7bee1fa1a8244b0f78d54ea9c9
Will <w.j.colgate@gmail.com>
2026-01-20 12:36:37 +0700

    completed loops

--- src/gas-optis/g01-loops/Optimized.sol
+++ src/gas-optis/g01-loops/Optimized.sol
@@ -4,22 +4,64 @@ pragma solidity 0.8.20;
 import {ILoops} from "./Common.sol";
 
 contract Loops is ILoops {
-    function loop1(uint256[] calldata array) external pure returns (uint256 result) {
-        for (uint256 i = 0; i < array.length; i++) {
-            result += array[i];
+    function loop1(
+        uint256[] calldata array
+    ) external pure returns (uint256 result) {
+        assembly {
+            let len := array.length // Load array length
+            let ptr := array.offset // Get base offset of the array data
+
+            for {
+                let i := 0
+            } lt(i, len) {
+                i := add(i, 1)
+            } {
+                // Read element at (ptr + i*32)
+                let element := calldataload(add(ptr, mul(i, 0x20)))
+                result := add(result, element)
+            }
         }
     }
 
-    function loop2(uint256[10] calldata array) external pure returns (uint256 result) {
-        for (uint256 i = 0; i < array.length; i++) {
-            result += array[i];
+    function loop2(
+        uint256[10] calldata array
+    ) external pure returns (uint256 result) {
+        assembly {
+            for {
+                let i := 0
+            } lt(i, 10) {
+                i := add(i, 1)
+            } {
+                // array points to array[0] as no other function param. Offset by 32 bytes
+                // for each loop
+                let element := calldataload(add(array, mul(i, 0x20)))
+                result := add(result, element)
+            }
         }
     }
 
-    function loop3(uint256[] calldata array) external pure returns (uint256 result) {
-        require(array.length <= 10);
-        for (uint256 i = 0; i < array.length; i++) {
-            result += array[i];
+    function loop3(
+        uint256[] calldata array
+    ) external pure returns (uint256 result) {
+        assembly {
+            let len := array.length // Load array length
+
+            // Check if len is greater than 10 and revert if true
+            if gt(len, 10) {
+                revert(0, 0)
+            }
+
+            let ptr := array.offset // Get base offset of the array data
+
+            for {
+                let i := 0
+            } lt(i, len) {
+                i := add(i, 1)
+            } {
+                // Read element at (ptr + i*32)
+                let element := calldataload(add(ptr, mul(i, 0x20)))
+                result := add(result, element)
+            }
         }
     }
 }
